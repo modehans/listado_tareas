fcc48342005cc9b0b7aa6dbb531a7db977e4beff
modehans <monicaglezsanch@gmail.com>
2022-08-24 10:01:34 +0200

    Change Index.js from githubpages

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
